363e475e64be631916d61408d20fbce9304cdb7a
Broktoon <davidcurren@gmail.com>
2026-02-20 08:47:48 -0500

    Fixing links after file folder structure change

--- js/app.js
+++ js/app.js
@@ -12,12 +12,12 @@
 */
 
 // Fixed paths:
-const POINTS_URL = "data/points.json";
-const AT_GEOJSON_URL = "data/appalachian-trail.geojson";
+const POINTS_URL = "/data/points.json";
+const AT_GEOJSON_URL = "/data/appalachian-trail.geojson";
 
 // Option B (precomputed normals) single-file dataset:
 // Build this file locally using tools/build_planning_normals.js (included below in this response).
-const NORMALS_URL = "data/planning_normals.json";
+const NORMALS_URL = "/data/planning_normals.json";
 
 // Open-Meteo endpoints
 const FORECAST_BASE = "https://api.open-meteo.com/v1/forecast";
